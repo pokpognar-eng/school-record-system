6042cb311a3591cd11a7a8066c8d37220aeab0ea
POK <email@example.com>
2025-12-17 02:36:15 +0700

    Force downgrade to Tailwind v3 clean install

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,6 @@ export default {
       fontFamily: {
         sans: ['Sarabun', 'sans-serif'],
       },
-      animation: {
-        'fade-in': 'fadeIn 0.5s ease-out',
-      },
-      keyframes: {
-        fadeIn: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
-        }
-      }
     },
   },
   plugins: [],
